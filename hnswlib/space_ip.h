0e9e0234025076468f0333e68cdd0394a6fa7234
Yury Malkov <yurymalkov@mail.ru>
2018-03-16 16:29:23 +0300

    update tests for inner product, 16-base

--- hnswlib/space_ip.h
+++ hnswlib/space_ip.h
@@ -30,11 +30,9 @@ namespace hnswlib {
         for (int i = 0; i < qty; i++) {
             res += -((float *) pVect1)[i] * ((float *) pVect2)[i];
         }
-        return (1.0f-res);
+        return (1.0f - res);
 
     };
-
-
 
 
     static float
@@ -81,8 +79,7 @@ namespace hnswlib {
         }
 
         _mm_store_ps(TmpRes, sum_prod);
-        float sum = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];
-        ;
+        float sum = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];;
         return 1.0f - sum;
 #else
         size_t qty16 = qty / 16;
@@ -134,22 +131,97 @@ namespace hnswlib {
         return 1.0f - sum;
 #endif
     };
+    static float
+    InnerProductSIMD16Ext(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
+        float PORTABLE_ALIGN32 TmpRes[8];
+        float *pVect1 = (float *) pVect1v;
+        float *pVect2 = (float *) pVect2v;
+        size_t qty = *((size_t *) qty_ptr);
 
-    class L2Space : public SpaceInterface<float> {
+#ifdef USE_AVX
+        size_t qty16 = qty / 16;
+        size_t qty4 = qty / 4;
+
+        const float *pEnd1 = pVect1 + 16 * qty16;
+        const float *pEnd2 = pVect1 + 4 * qty4;
+
+        __m256 sum256 = _mm256_set1_ps(0);
+
+        while (pVect1 < pEnd1) {
+            //_mm_prefetch((char*)(pVect2 + 16), _MM_HINT_T0);
+
+            __m256 v1 = _mm256_loadu_ps(pVect1);
+            pVect1 += 8;
+            __m256 v2 = _mm256_loadu_ps(pVect2);
+            pVect2 += 8;
+            sum256 = _mm256_add_ps(sum256, _mm256_mul_ps(v1, v2));
+
+            v1 = _mm256_loadu_ps(pVect1);
+            pVect1 += 8;
+            v2 = _mm256_loadu_ps(pVect2);
+            pVect2 += 8;
+            sum256 = _mm256_add_ps(sum256, _mm256_mul_ps(v1, v2));
+        }
+
+        _mm256_store_ps(TmpRes, sum256);
+        float sum = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
+
+        return 1.0f - sum;
+#else
+        size_t qty16 = qty / 16;
+        size_t qty4 = qty / 4;
+
+        const float *pEnd1 = pVect1 + 16 * qty16;
+        const float *pEnd2 = pVect1 + 4 * qty4;
+
+        __m128 v1, v2;
+        __m128 sum_prod = _mm_set1_ps(0);
+
+        while (pVect1 < pEnd1) {
+            v1 = _mm_loadu_ps(pVect1);
+            pVect1 += 4;
+            v2 = _mm_loadu_ps(pVect2);
+            pVect2 += 4;
+            sum_prod = _mm_add_ps(sum_prod, _mm_mul_ps(v1, v2));
+
+            v1 = _mm_loadu_ps(pVect1);
+            pVect1 += 4;
+            v2 = _mm_loadu_ps(pVect2);
+            pVect2 += 4;
+            sum_prod = _mm_add_ps(sum_prod, _mm_mul_ps(v1, v2));
+
+            v1 = _mm_loadu_ps(pVect1);
+            pVect1 += 4;
+            v2 = _mm_loadu_ps(pVect2);
+            pVect2 += 4;
+            sum_prod = _mm_add_ps(sum_prod, _mm_mul_ps(v1, v2));
+
+            v1 = _mm_loadu_ps(pVect1);
+            pVect1 += 4;
+            v2 = _mm_loadu_ps(pVect2);
+            pVect2 += 4;
+            sum_prod = _mm_add_ps(sum_prod, _mm_mul_ps(v1, v2));
+        }
+        _mm_store_ps(TmpRes, sum_prod);
+        float sum = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];
+
+        return 1.0f - sum;
+#endif
+    };
+
+
+    class InnerProductSpace : public SpaceInterface<float> {
 
         DISTFUNC<float> fstdistfunc_;
         size_t data_size_;
         size_t dim_;
     public:
-        L2Space(size_t dim) {
+        InnerProductSpace(size_t dim) {
             fstdistfunc_ = InnerProduct;
             if (dim % 4 == 0)
                 fstdistfunc_ = InnerProductSIMD4Ext;
-            //if (dim % 16 == 0)
-            //    fstdistfunc_ = InnerProductSIMD16Ext;
-            /*else{
-                throw runtime_error("Data type not supported!");
-            }*/
+            if (dim % 16 == 0)
+                fstdistfunc_ = InnerProductSIMD16Ext;
             dim_ = dim;
             data_size_ = dim * sizeof(float);
         }
@@ -167,6 +239,6 @@ namespace hnswlib {
         }
 
     };
-    
+
 
 };
