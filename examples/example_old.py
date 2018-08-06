ffa17bde3e0b27d7296356eba0c97c9c9d9a82ba
Yury Malkov <yurymalkov@mail.ru>
2018-08-06 19:02:28 +0300

    add partial saving/loading

--- examples/example_old.py
+++ examples/example_old.py
@@ -7,33 +7,28 @@ num_elements = 10000
 # Generating sample data
 data = np.float32(np.random.random((num_elements, dim)))
 
-# We split the data in two batches:
-data1 = data[:num_elements // 2]
-data2 = data[num_elements // 2:]
-
 # Declaring index
 p = hnswlib.Index(space='l2', dim=dim)  # possible options are l2, cosine or ip
 
 # Initing index
-# max_elements - the maximum number of elements (capacity). Will throw an exception if exceeded
-# during insertion of an element.
-# The capacity can be increased by saving/loading the index, see below.
+# max_elements - the maximum number of elements, should be known beforehand
+#     (probably will be made optional in the future)
 #
 # ef_construction - controls index search speed/build speed tradeoff
-#
-# M - is tightly connected with internal dimensionality of the data. Strongly affects the memory consumption (~M)
-# Higher M leads to higher accuracy/run_time at fixed ef/efConstruction
+# M - is tightly connected with internal dimensionality of the data
+#     stronlgy affects the memory consumption
 
-p.init_index(max_elements=num_elements//2, ef_construction=100, M=16)
+p.init_index(max_elements=num_elements, ef_construction=100, M=16)
 
 # Controlling the recall by setting ef:
 # higher ef leads to better accuracy, but slower search
 p.set_ef(10)
 
-# Set number of threads used during batch search/construction
-# By default using all available cores
-p.set_num_threads(4)
+p.set_num_threads(4)  # by default using all available cores
 
+# We split the data in two batches:
+data1 = data[:num_elements // 2]
+data2 = data[num_elements // 2:]
 
 print("Adding first batch of %d elements" % (len(data1)))
 p.add_items(data1)
@@ -49,12 +44,10 @@ p.save_index("first_half.bin")
 del p
 
 # Reiniting, loading the index
-p = hnswlib.Index(space='l2', dim=dim)  # the space can be changed - keeps the data, alters the distance function.
+p = hnswlib.Index(space='l2', dim=dim)  # you can change the sa
 
 print("\nLoading index from 'first_half.bin'\n")
-
-# Increase the total capacity (max_elements), so that it will handle the new data
-p.load_index("first_half.bin", max_elements = num_elements)
+p.load_index("first_half.bin")
 
 print("Adding the second batch of %d elements" % (len(data2)))
 p.add_items(data2)
