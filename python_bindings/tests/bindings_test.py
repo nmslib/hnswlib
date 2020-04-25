8388736ff3a8cb3bdc9bbb5fbbe8006a6c4d82bf
Yury Malkov <yurymalkov@mail.ru>
2020-04-25 15:47:31 -0700

    add test for some random vector dim/spaces

--- python_bindings/tests/bindings_test.py
+++ python_bindings/tests/bindings_test.py
@@ -3,64 +3,65 @@ import unittest
 
 class RandomSelfTestCase(unittest.TestCase):
     def testRandomSelf(self):
-        import hnswlib
-        import numpy as np
+        for dim in [3,4,7,8,12,16,19,20,23,29,32,33]:
+            for space in ['l2', 'cosine']:
+                import hnswlib
+                import numpy as np
+                
+                num_elements = 10000
 
-        dim = 16
-        num_elements = 10000
+                # Generating sample data
+                data = np.float32(np.random.random((num_elements, dim)))
 
-        # Generating sample data
-        data = np.float32(np.random.random((num_elements, dim)))
+                # Declaring index
+                p = hnswlib.Index(space=space, dim=dim)  # possible options are l2, cosine or ip
 
-        # Declaring index
-        p = hnswlib.Index(space='l2', dim=dim)  # possible options are l2, cosine or ip
+                # Initing index
+                # max_elements - the maximum number of elements, should be known beforehand
+                #     (probably will be made optional in the future)
+                #
+                # ef_construction - controls index search speed/build speed tradeoff
+                # M - is tightly connected with internal dimensionality of the data
+                #     stronlgy affects the memory consumption
 
-        # Initing index
-        # max_elements - the maximum number of elements, should be known beforehand
-        #     (probably will be made optional in the future)
-        #
-        # ef_construction - controls index search speed/build speed tradeoff
-        # M - is tightly connected with internal dimensionality of the data
-        #     stronlgy affects the memory consumption
+                p.init_index(max_elements = num_elements, ef_construction = 100, M = 16)
 
-        p.init_index(max_elements = num_elements, ef_construction = 100, M = 16)
+                # Controlling the recall by setting ef:
+                # higher ef leads to better accuracy, but slower search
+                p.set_ef(20)
 
-        # Controlling the recall by setting ef:
-        # higher ef leads to better accuracy, but slower search
-        p.set_ef(10)
+                p.set_num_threads(4)  # by default using all available cores
 
-        p.set_num_threads(4)  # by default using all available cores
+                # We split the data in two batches:
+                data1 = data[:num_elements // 2]
+                data2 = data[num_elements // 2:]
 
-        # We split the data in two batches:
-        data1 = data[:num_elements // 2]
-        data2 = data[num_elements // 2:]
+                print("Adding first batch of %d elements" % (len(data1)))
+                p.add_items(data1)
 
-        print("Adding first batch of %d elements" % (len(data1)))
-        p.add_items(data1)
+                # Query the elements for themselves and measure recall:
+                labels, distances = p.knn_query(data1, k=1)
+                self.assertAlmostEqual(np.mean(labels.reshape(-1) == np.arange(len(data1))),1.0,3)
 
-        # Query the elements for themselves and measure recall:
-        labels, distances = p.knn_query(data1, k=1)
-        self.assertAlmostEqual(np.mean(labels.reshape(-1) == np.arange(len(data1))),1.0,3)
+                # Serializing and deleting the index:
+                index_path='first_half.bin'
+                print("Saving index to '%s'" % index_path)
+                p.save_index("first_half.bin")
+                del p
 
-        # Serializing and deleting the index:
-        index_path='first_half.bin'
-        print("Saving index to '%s'" % index_path)
-        p.save_index("first_half.bin")
-        del p
+                # Reiniting, loading the index
+                p = hnswlib.Index(space='l2', dim=dim)  # you can change the sa
 
-        # Reiniting, loading the index
-        p = hnswlib.Index(space='l2', dim=dim)  # you can change the sa
+                print("\nLoading index from 'first_half.bin'\n")
+                p.load_index("first_half.bin")
 
-        print("\nLoading index from 'first_half.bin'\n")
-        p.load_index("first_half.bin")
+                print("Adding the second batch of %d elements" % (len(data2)))
+                p.add_items(data2)
 
-        print("Adding the second batch of %d elements" % (len(data2)))
-        p.add_items(data2)
+                # Query the elements for themselves and measure recall:
+                labels, distances = p.knn_query(data, k=1)
 
-        # Query the elements for themselves and measure recall:
-        labels, distances = p.knn_query(data, k=1)
-
-        self.assertAlmostEqual(np.mean(labels.reshape(-1) == np.arange(len(data))),1.0,3)
+                self.assertAlmostEqual(np.mean(labels.reshape(-1) == np.arange(len(data))),1.0,3)
 
 
 if __name__ == "__main__":
