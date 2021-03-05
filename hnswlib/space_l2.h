d2ba4155004dd51b98ef51a2e73096b689f470b7
Connelly Barnes <cobarnes@Connellys-MacBook-Pro.local>
2021-03-05 12:46:00 -0800

    make L2SpaceI::fstdistfunc_ be protected so it can be subclassed and the distance function can be modified more easily

--- hnswlib/space_l2.h
+++ hnswlib/space_l2.h
@@ -247,7 +247,9 @@ namespace hnswlib {
 
     class L2SpaceI : public SpaceInterface<int> {
 
+	protected:
         DISTFUNC<int> fstdistfunc_;
+	private:
         size_t data_size_;
         size_t dim_;
     public:
