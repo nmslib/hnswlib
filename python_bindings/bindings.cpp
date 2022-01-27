1078891dd28f30264a28cb3fc287462cf11da066
Peter Sobot <psobot@spotify.com>
2022-01-27 15:13:08 -0500

    Rename uint to unsigned int for MSVC.

--- python_bindings/bindings.cpp
+++ python_bindings/bindings.cpp
@@ -848,7 +848,7 @@ template<> const std::string typeName<unsigned char>() { return "uint8"; }
 template<> const std::string typeName<short>() { return "int16"; }
 template<> const std::string typeName<unsigned short>() { return "uint16"; }
 template<> const std::string typeName<int>() { return "int32"; }
-template<> const std::string typeName<uint>() { return "uint32"; }
+template<> const std::string typeName<unsigned int>() { return "uint32"; }
 template<> const std::string typeName<float>() { return "float32"; }
 template<> const std::string typeName<long>() { return "int64"; }
 template<> const std::string typeName<unsigned long>() { return "uint64"; }
