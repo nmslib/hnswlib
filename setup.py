eacbe219e28ba5de7180e2e45a5c04b0ab2fd375
eric schkufza <eric.schkufza@gmail.com>
2022-08-05 11:44:44 -0700

    link deps
    Signed-off-by: eric schkufza <eric.schkufza@gmail.com>

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ import pybind11
 import setuptools
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
+from distutils.dep_util import newer_group
+from distutils import log
 
 __version__ = '0.6.1'
 
@@ -89,11 +91,12 @@ class BuildExt(build_ext):
         'msvc': [],
     }
 
+    # NOTE(eschkufz): I've changed this min from 10.7 to 11.1
     if sys.platform == 'darwin':
         if platform.machine() == 'arm64':
             c_opts['unix'].remove('-march=native')
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        link_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=11.1']
+        link_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=11.1']
     else:
         c_opts['unix'].append("-fopenmp")
         link_opts['unix'].extend(['-fopenmp', '-pthread'])
@@ -112,16 +115,95 @@ class BuildExt(build_ext):
         for ext in self.extensions:
             ext.extra_compile_args.extend(opts)
             ext.extra_link_args.extend(self.link_opts.get(ct, []))
-            ext.extra_compile_args.extend(['-L/usr/local/lib', '-laws-cpp-sdk-core', '-laws-cpp-sdk-s3'])
+            # NOTE(eschkufz): I've added dependencies on aws
+            ext.libraries = ['aws-cpp-sdk-s3', 'aws-cpp-sdk-core']
 
         build_ext.build_extensions(self)
 
+    # TODO(eschkufz): DELETE ME
     def build_extension(self, ext):
         print("BUILD EXTENSION...")
-        print(ext.sources)
-        print(ext.extra_compile_args)
-        print(self.get_libraries(ext))
-        super().build_extension(ext)
+        sources = ext.sources
+        if sources is None or not isinstance(sources, (list, tuple)):
+            raise DistutilsSetupError(
+                "in 'ext_modules' option (extension '%s'), "
+                "'sources' must be present and must be "
+                "a list of source filenames" % ext.name)
+        # sort to make the resulting .so file build reproducible
+        sources = sorted(sources)
+
+        ext_path = self.get_ext_fullpath(ext.name)
+        depends = sources + ext.depends
+        if not (self.force or newer_group(depends, ext_path, 'newer')):
+            print("HERE?")
+            log.debug("skipping '%s' extension (up-to-date)", ext.name)
+            return
+        else:
+            log.info("building '%s' extension", ext.name)
+
+        # First, scan the sources for SWIG definition files (.i), run
+        # SWIG on 'em to create .c files, and modify the sources list
+        # accordingly.
+        sources = self.swig_sources(sources, ext)
+
+        # Next, compile the source code to object files.
+
+        # XXX not honouring 'define_macros' or 'undef_macros' -- the
+        # CCompiler API needs to change to accommodate this, and I
+        # want to do one thing at a time!
+
+        # Two possible sources for extra compiler arguments:
+        #   - 'extra_compile_args' in Extension object
+        #   - CFLAGS environment variable (not particularly
+        #     elegant, but people seem to expect it and I
+        #     guess it's useful)
+        # The environment variable should take precedence, and
+        # any sensible compiler will give precedence to later
+        # command line args.  Hence we combine them in order:
+        extra_args = ext.extra_compile_args or []
+
+        macros = ext.define_macros[:]
+        for undef in ext.undef_macros:
+            macros.append((undef,))
+
+        objects = self.compiler.compile(sources,
+                                        output_dir=self.build_temp,
+                                        macros=macros,
+                                        include_dirs=ext.include_dirs,
+                                        debug=self.debug,
+                                        extra_postargs=extra_args,
+                                        depends=ext.depends)
+
+        # XXX outdated variable, kept here in case third-part code
+        # needs it.
+        self._built_objects = objects[:]
+
+        print(f"objects = {objects}")
+
+        # Now link the object files together into a "shared object" --
+        # of course, first we have to figure out all the other things
+        # that go into the mix.
+        if ext.extra_objects:
+            objects.extend(ext.extra_objects)
+        extra_args = ext.extra_link_args or []
+
+        # Detect target language, if not provided
+        language = ext.language or self.compiler.detect_language(sources)
+
+        print(f"self.get_libraries(ext) = {self.get_libraries(ext)}")
+        libs = self.get_libraries(ext)
+        #libs.extend(['aws-cpp-sdk-s3', 'aws-cpp-sdk-core']) # This is where the dylibs need to go
+
+        self.compiler.link_shared_object(
+            objects, ext_path,
+            libraries=libs,
+            library_dirs=ext.library_dirs,
+            runtime_library_dirs=ext.runtime_library_dirs,
+            extra_postargs=extra_args,
+            export_symbols=self.get_export_symbols(ext),
+            debug=self.debug,
+            build_temp=self.build_temp,
+            target_lang=language)
 
 setup(
     name='hnswlib',
