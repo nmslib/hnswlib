302f36a65566d6c6daba3259331e031772077193
hammadb <hammad@trychroma.com>
2023-06-08 15:49:29 -0700

    Make std::vector thread_local to prevent reallocation

--- hnswlib/hnswalg.h
+++ hnswlib/hnswalg.h
@@ -1168,8 +1168,7 @@ class HierarchicalNSW : public AlgorithmInterface<dist_t> {
         // Initialisation of the data and label and if appropriate the length
         const void* normalized_vector = data_point;
         size_t dim = *((size_t *) dist_func_param_);
-        // TODO: don't allocate memory for norm_array on every addPoint call
-        std::vector<float> norm_array(dim);
+        thread_local std::vector<float> norm_array(dim);
         if (normalize_) {
             float length = normalize_vector((float*)data_point, norm_array.data(), dim);
             void* lengthPtr = length_memory_ + cur_c * sizeof(float);
