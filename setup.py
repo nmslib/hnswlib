9b9a139bdf97bb790b871bc6f64a5795e4b3d154
Luke VanderHart <luke@vanderhart.net>
2023-09-02 22:02:07 -0400

    build before deploying sdist to setup deps

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-__version__ = '0.7.3.alpha3'
+__version__ = '0.7.3.alpha4'
 
 include_dirs = [
     pybind11.get_include(),
