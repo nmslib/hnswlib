5b3d81bed46330b7194f096d56e03c4d4be1d111
James Melville <jlmelville@gmail.com>
2023-09-17 16:05:56 -0700

    define a macro for error stream

--- hnswlib/hnswlib.h
+++ hnswlib/hnswlib.h
@@ -1,4 +1,11 @@
 #pragma once
+
+#ifndef HNSWLIB_ERR_OVERRIDE
+  #define HNSWERR std::cerr
+#else
+  #define HNSWERR HNSWLIB_ERR_OVERRIDE
+#endif
+
 #ifndef NO_MANUAL_VECTORIZATION
 #if (defined(__SSE__) || _M_IX86_FP > 0 || defined(_M_AMD64) || defined(_M_X64))
 #define USE_SSE
